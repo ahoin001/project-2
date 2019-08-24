e9ba0b59e82498248e488313e16be7c843441afe
Alexander Hoinville <ahoin001@gmail.com>
2019-08-24 15:27:35 -0400

    Add Route for login , need to add passport

--- Project/routes/authorization-routes.js
+++ Project/routes/authorization-routes.js
@@ -12,25 +12,27 @@ router.get("/signup", (req, res, next) => {
   res.render("authorization-views/sign-up");
 });
 
-// Make sure sign up form action posts to this url
+// Sign-Up form action posts to this url
 router.post("/signup", (req, res, next) => {
 
-  // username and password from form
+  // Information from form
   const username = req.body.username;
   const password = req.body.password;
+  const email = req.body.email;
 
   // bcrypting the password
   const salt = bcrypt.genSaltSync(10);
   const hashPass = bcrypt.hashSync(password, salt);
 
+  // TODO: Validation error message needs to be presented properly
   if (username === "" || password === "") {
     res.render("authorization-views/signup", {
-      errorMessage: "Indicate a username and a password to sign up"
+      errorMessage: "Username and password can not be blank for sign up"
     });
     return;
   }
 
-  // Creates and inserts new user into DB
+  // Creates and inserts new User into DB
   //TODO : Do we want email / image?
   User.create({
     username,
@@ -39,12 +41,46 @@ router.post("/signup", (req, res, next) => {
     // image: ''
   })
     .then(() => {
-      // redirect user to home page
-      res.redirect("/");
+      // redirect user to login page after successful account creation
+      res.redirect("/login");
     })
     .catch(error => {
+      // error on failed sign up
       console.log(error);
+      console.log(`Error creating/inserting user`)
     })
+
 });
 
+router.get("/login", (req, res, next) => {
+  res.render("authorization-views/login");
+});
+
+router.post("/signup", (req, res, next) => {
+
+  // Information from form
+  const username = req.body.username;
+  const password = req.body.password;
+
+  // TODO : We can add login validation  
+
+  // Creates and inserts new User into DB
+  //TODO : Do we want email / image?
+  User.create({
+    username,
+    password: hashPass
+    // email: '',
+    // image: ''
+  })
+    .then(() => {
+      // redirect user to login page after successful account creation
+      res.redirect("/login");
+    })
+    .catch(error => {
+      // error on failed sign up
+      console.log(error);
+      console.log(`Error creating/inserting user`)
+    })
+
+});
 module.exports = router;
