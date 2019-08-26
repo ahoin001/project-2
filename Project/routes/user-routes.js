511b0661ba440f91374899a03a3b176e00fa2265
Alexander Hoinville <ahoin001@gmail.com>
2019-08-26 19:29:37 -0400

    Added navabar for page traversal

--- Project/routes/user-routes.js
+++ Project/routes/user-routes.js
@@ -13,5 +13,23 @@ router.get("/user/:userid", (req, res, next) => {
   res.render("authorization-views/sign-up");
 });
 
+// GET route to display the form for editing/updating the user
+router.get("/user/:theId/edit", (req, res, next) => {
+  User
+    // req.params.theId will be whatever this.id is of the User from allUsers.hbs
+    .findById(req.params.theId)
+    .then(theUser => {
+     
+
+
+    })
+    .catch(err => console.log("Error while getting the User from DB: ", err));
+});
+
+router.post("/user/:userid/edit", (req, res, next) => {
+
+
+  res.render("user-views/edit");
+});
 
 module.exports = router;
