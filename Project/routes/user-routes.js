c8b40e511631d867d740df0f65d0349390b9d9f2
Alexander Hoinville <ahoin001@gmail.com>
2019-08-28 20:40:12 -0400

    Begin adding profile pic update feature

--- Project/routes/user-routes.js
+++ Project/routes/user-routes.js
@@ -22,9 +22,11 @@ router.get("/user-page/:id/edit", ensureLogin.ensureLoggedIn(), (req, res) => {
 router.post("/user/:id/update", ensureLogin.ensureLoggedIn('/'), (req, res) => {
 
   // Find user in DB using current user ID , and update the username to what is in the form
+  
+
   User
   // TODO :Can add whatever else user should be able to update here
-    .findByIdAndUpdate(req.params.id, { username: req.body.username })
+    .findByIdAndUpdate(req.params.id, { username: req.body.username, profilepic: req.file.url })
     .then(() => {
       res.redirect('/user-page');
     })
